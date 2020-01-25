5910c3faff1684e41eaa344a55da3ac26e14bfde
MilshynVitalii <milshyn.vitalii@gmail.com>
2020-01-25 16:11:18 +0200

    header onscroll hidden

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-$(".special-offer__slider").slick({
-  infinite: true,
-  slidesToShow: 4,
-  slidesToScroll: 1
+window.addEventListener("DOMContentLoaded", function() {
+  let header = document.querySelector(".header");
+  document.addEventListener("scroll", function(event) {
+    if (window.pageYOffset > 0) {
+      header.style.boxShadow = "0px 0 8px -2px #001e28";
+    } else {
+      header.style.boxShadow = "";
+    }
+    if (window.pageYOffset > 360) {
+      header.style.transform = "translateY(-110%)";
+    } else {
+      header.style.transform = "translateY(0%)";
+    }
+  });
 });
